07e46d078d02a81f35d1228278e9df6330afb0e3
Rahul Nene <rahul.nene@live.com>
2023-08-02 10:55:35 +0530

    Day 1 cleanup

--- src/day1.rs
+++ src/day1.rs
@@ -2,39 +2,28 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-#[derive(Debug, Clone, Copy)]
-struct Elf {
-    index: u32,
-    calories: u32,
-}
-
-fn main() {
+pub fn solution() {
     if let Ok(lines) = read_lines("./problem1.txt") {
-        let mut current_elf = Elf {
-            index: 0,
-            calories: 0,
-        };
+        let mut current_elf_calories = 0;
         let mut max_calories: Vec<u32> = vec![0; 3];
-        for line in lines {
-            if let Ok(food_information) = line {
-                match food_information.as_str() {
-                    "" => {
-                        current_elf = Elf {
-                            index: current_elf.index + 1,
-                            calories: 0,
-                        }
-                    }
-                    _ => current_elf.calories += food_information.parse::<u32>().unwrap(),
+        for line in lines.flatten() {
+            match line.as_str() {
+                "" => {
+                    current_elf_calories = 0;
                 }
-                if current_elf.calories > max_calories[0] {
-                    max_calories.push(current_elf.calories);
-                    max_calories.remove(0);
-                    max_calories.sort_unstable();
-                }
+                _ => current_elf_calories += line.parse::<u32>().unwrap(),
+            }
+            if current_elf_calories > max_calories[0] {
+                max_calories.push(current_elf_calories);
+                max_calories.remove(0);
+                max_calories.sort_unstable();
             }
         }
-        println!("Calorie sum is {:?}", max_calories.iter().sum::<u32>());
-        dbg!(max_calories);
+        println!("Maximum calories carried is {:?}", max_calories[2]);
+        println!(
+            "Calories carried by top 3 elves is {:?}",
+            max_calories.iter().sum::<u32>()
+        );
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod day1;
+
 fn main() {
-    
+    day1::solution();
 }
