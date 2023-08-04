aa03eca696a69c169a03f0a97c07628bc23599a0
Rahul Nene <rahul.nene@live.com>
2023-08-04 14:22:58 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,29 @@ pub mod util;
 
 fn main() {
     let start = std::time::Instant::now();
-    // println!("The solution to part 1 is: {}", days::day6::solution(1));
+    println!("The solution to part 1 is: {}", days::day1::solution(1));
+    println!("The solution to part 2 is: {}", days::day1::solution(2));
+    
+    println!("The solution to part 1 is: {}", days::day2::solution(1));
+    println!("The solution to part 2 is: {}", days::day2::solution(2));
+    
+    println!("The solution to part 1 is: {}", days::day3::solution(1));
+    println!("The solution to part 2 is: {}", days::day3::solution(2));
+    
+    println!("The solution to part 1 is: {}", days::day4::solution(1));
+    println!("The solution to part 2 is: {}", days::day4::solution(2));
+    
+    println!("The solution to part 1 is: {}", days::day6::solution(1));
     println!("The solution to part 2 is: {}", days::day6::solution(2));
+    
+    println!("The solution to part 1 is: {}", days::day10::solution());
+    
+    println!("The solution to part 1 is: {}", days::day11::solution(1));
+
+    println!("The solution to part 1 is: {}", days::day13::solution(1));
+    println!("The solution to part 2 is: {}", days::day13::solution(2));
+
+    
+    println!("The solution to part 1 is: {}", days::day18::solution(1));
     println!("Time elapsed: {:?}", start.elapsed());
 }
