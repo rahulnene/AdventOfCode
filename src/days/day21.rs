f7f2ae765bb8767f7e0023b5d976619e2325327f
Rahul Nene <rahul.nene@live.com>
2023-08-08 14:33:56 +0530

    day21p2

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -1,116 +1,146 @@
-use std::fmt::{self, Debug, Display};
-use std::time::Instant;
+use std::collections::{HashMap, VecDeque};
 
 pub fn solution(part: u8) -> isize {
-    let now = Instant::now();
     let lines = include_str!("../../problem_inputs/day21.txt");
-    let monkeys = parse_input(lines);
-    let root = monkeys.iter().find(|m| m.name == "root").unwrap();
-    // let left_monkey = &root.value;
-    // dbg!(left_monkey);
-    let part_1 = resolve(root, &Box::new(&monkeys));
-    println!("Time: {}ms", now.elapsed().as_millis());
-    for monkey in monkeys {
-        if monkey.name == "humn" {}
-    }
-
     match part {
-        1 => part_1,
-        2 => 2,
-        _ => 0,
+        1 => solve01(lines),
+        2 => solve02(lines),
+        _ => -1,
     }
 }
 
-fn parse_input(lines: &str) -> Vec<Monkey> {
-    let mut monkeys = Vec::new();
-    for line in lines.split("\n") {
-        let name = line[0..4].to_string();
-        let rest = &line[6..];
-        if !rest.contains(" ") {
-            monkeys.push(Monkey {
-                name,
-                value: Box::new(Value::Val(rest.parse::<isize>().unwrap())),
-            });
-        } else {
-            let mut split = rest.split(" ");
-            let left = split.next().unwrap().to_string();
-            let op = match split.next().unwrap() {
-                "+" => Box::new(|a: isize, b: isize| a + b),
-                "-" => Box::new(|a: isize, b: isize| a - b) as Box<dyn Fn(isize, isize) -> isize>,
-                "*" => Box::new(|a: isize, b: isize| a * b),
-                "/" => Box::new(|a: isize, b: isize| a / b),
-                _ => panic!("Invalid operation"),
-            };
-            let right = split.next().unwrap().to_string();
-            monkeys.push(Monkey {
-                name,
-                value: Box::new(Value::Operation((
-                    Monkey {
-                        name: left,
-                        value: Box::new(Value::Val(0)),
-                    },
-                    op,
-                    Monkey {
-                        name: right,
-                        value: Box::new(Value::Val(0)),
-                    },
-                ))),
-            });
+pub fn solve01(lines: &str) -> isize {
+    let now = std::time::Instant::now();
+    let (monkeys, root_index, _) = parse(lines);
+    println!("Time taken: {:?}", now.elapsed());
+    monkeys[root_index].get_value(&monkeys)
+}
+
+pub fn solve02(lines: &str) -> isize {
+    let now = std::time::Instant::now();
+    let (monkeys, root_index, humn_index) = parse(lines);
+    let mut queue: VecDeque<(usize, isize)> = VecDeque::new();
+    if let Monkey::Expression(i1, i2, _) = monkeys[root_index] {
+        queue.push_back((i2, monkeys[i1].get_value(&monkeys)));
+        queue.push_back((i1, monkeys[i2].get_value(&monkeys)));
+    }
+
+    while let Some((i, expected)) = queue.pop_front() {
+        if i == humn_index {
+            println!("Time taken: {:?}", now.elapsed());
+            return expected;
+        }
+
+        if let Monkey::Expression(i1, i2, _) = monkeys[i] {
+            queue.push_back((i1, monkeys[i].get_expected1(expected, &monkeys)));
+            queue.push_back((i2, monkeys[i].get_expected2(expected, &monkeys)));
         }
     }
-    let monkeys_ptr = Box::new(monkeys);
-    *monkeys_ptr
+    println!("Time Taken: {:?}", now.elapsed());
+    -1
 }
 
-fn resolve(current_monkey: &Monkey, monkeys: &Box<&Vec<Monkey>>) -> isize {
-    match current_monkey.value.as_ref() {
-        Value::Val(val) => *val,
-        Value::Operation((left_monkey, op, right_monkey)) => {
-            let left_monkey = monkeys
-                .iter()
-                .find(|m| m.name == *left_monkey.name)
-                .unwrap();
-            let right_monkey = monkeys
-                .iter()
-                .find(|m| m.name == *right_monkey.name)
-                .unwrap();
-            let left_val = resolve(left_monkey, &monkeys);
-            let right_val = resolve(right_monkey, &monkeys);
-            op(left_val, right_val)
-        }
-    }
-}
-enum Value {
-    Val(isize),
-    Operation((Monkey, Box<dyn Fn(isize, isize) -> isize>, Monkey)),
+fn parse(lines: &str) -> (Vec<Monkey>, usize, usize) {
+    let id_to_index: HashMap<&str, usize> = HashMap::from_iter(
+        lines
+            .lines()
+            .enumerate()
+            .map(|(i, line)| (line.split_once(':').map(|(id, _)| id).unwrap(), i)),
+    );
+
+    (
+        Vec::from_iter(lines.lines().map(|line| {
+            let (_, definition) = line.split_once(": ").unwrap();
+            match definition.parse::<isize>() {
+                Ok(c) => Monkey::Const(c),
+                Err(_) => {
+                    let mut parts = definition.split(' ');
+                    let i1 = id_to_index.get(parts.next().unwrap()).unwrap();
+                    let operator = match parts.next().unwrap() {
+                        "+" => Operator::Add,
+                        "-" => Operator::Sub,
+                        "*" => Operator::Mul,
+                        "/" => Operator::Div,
+                        _ => panic!("bad input"),
+                    };
+                    let i2 = id_to_index.get(parts.next().unwrap()).unwrap();
+                    Monkey::Expression(*i1, *i2, operator)
+                }
+            }
+        })),
+        *id_to_index.get("root").unwrap(),
+        *id_to_index.get("humn").unwrap(),
+    )
 }
 
-struct Monkey {
-    name: String,
-    value: Box<Value>,
+#[derive(Debug, Clone, Copy)]
+enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div,
 }
 
-impl Debug for Monkey {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Monkey {{ name: {}, value: {:?} }}",
-            self.name, self.value
-        )
-    }
-}
-
-impl Display for Monkey {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.name)
-    }
-}
-
-impl Debug for Value {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl Operator {
+    fn calculate(&self, v1: isize, v2: isize) -> isize {
+        use Operator::*;
         match self {
-            Value::Val(val) => write!(f, "Value::Val({})", val),
-            Value::Operation((l, _, r)) => write!(f, "{} {}", l, r),
+            Add => v1 + v2,
+            Sub => v1 - v2,
+            Mul => v1 * v2,
+            Div => v1.checked_div(v2).unwrap_or(0),
+        }
+    }
+
+    fn reverse_calculate(&self) -> Operator {
+        use Operator::*;
+        match self {
+            Add => Sub,
+            Sub => Add,
+            Mul => Div,
+            Div => Mul,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Monkey {
+    Const(isize),
+    Expression(usize, usize, Operator),
+}
+
+impl Monkey {
+    fn get_value(&self, monkeys: &[Monkey]) -> isize {
+        match self {
+            Monkey::Const(c) => *c,
+            Monkey::Expression(i1, i2, op) => op.calculate(
+                monkeys[*i1].get_value(monkeys),
+                monkeys[*i2].get_value(monkeys),
+            ),
+        }
+    }
+
+    fn get_expected1(&self, expected_result: isize, monkeys: &[Monkey]) -> isize {
+        match self {
+            Monkey::Const(_) => panic!(),
+            Monkey::Expression(_, i2, op) => op
+                .reverse_calculate()
+                .calculate(expected_result, monkeys[*i2].get_value(monkeys)),
+        }
+    }
+
+    fn get_expected2(&self, expected_result: isize, monkeys: &[Monkey]) -> isize {
+        match self {
+            Monkey::Const(_) => panic!(),
+            Monkey::Expression(i1, _, op) => {
+                let v1 = monkeys[*i1].get_value(monkeys);
+                match op {
+                    Operator::Add | Operator::Mul => {
+                        op.reverse_calculate().calculate(expected_result, v1)
+                    }
+                    Operator::Sub | Operator::Div => op.calculate(v1, expected_result),
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ pub mod util;
 
 fn main() {
     println!("The solution to part 1 is: {}", days::day21::solution(1));
-    // println!("The solution to part 2 is: {}", days::day21::solution(2));
+    println!("The solution to part 2 is: {}", days::day21::solution(2));
 }
