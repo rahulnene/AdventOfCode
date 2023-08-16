90d68a6b78d27782edb1c5299f69d632e6420732
Rahul Nene <rahul.nene@live.com>
2023-08-16 07:24:48 +0530

    Day 3, 4 complete

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ pub mod year_2020;
 fn main() {
     println!(
         "The solution to part 1 is: {}\n",
-        year_2020::days::day3::solution(1)
+        year_2020::days::day4::solution(1)
     );
     println!(
         "The solution to part 2 is: {}",
-        year_2020::days::day3::solution(2)
+        year_2020::days::day4::solution(2)
     );
 }
--- src/year_2020/days/day3.rs
+++ src/year_2020/days/day3.rs
@@ -1,16 +1,30 @@
+use itertools::Itertools;
+
 pub fn solution(part: u8) -> usize {
     let lines = include_str!("../../../problem_inputs_2020/day_3.txt");
     match part {
-        1 => solve01(lines),
+        1 => solve(lines, 1, 3),
         2 => solve02(lines),
         _ => 1,
     }
 }
 
-fn solve01(lines: &str) -> usize {
-    0
+fn solve(lines: &str, down: u8, right: u8) -> usize {
+    let mut count = 0;
+    let mut col = 0;
+    for line in lines.lines().step_by(down as usize) {
+        let width = line.len();
+        let row: Vec<bool> = line.chars().map(|c| (c == '#')).collect_vec();
+        count += row[col % width] as usize;
+        col += right as usize;
+    }
+    count
 }
 
 fn solve02(lines: &str) -> usize {
-    0
+    solve(lines, 2, 1)
+        * solve(lines, 1, 1)
+        * solve(lines, 1, 3)
+        * solve(lines, 1, 5)
+        * solve(lines, 1, 7)
 }
