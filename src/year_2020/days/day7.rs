e124392d9fa988e63edbae60d50bb752fb7f5822
Rahul Nene <rahul.nene@live.com>
2023-08-20 15:11:39 +0530

    Day 7 part 1 finished

--- src/year_2020/days/day7.rs
+++ src/year_2020/days/day7.rs
@@ -1,10 +1,10 @@
-use std::fmt::Debug;
-
 use fxhash::FxHashMap;
 use itertools::Itertools;
+use petgraph::algo::dijkstra;
+use petgraph::graph::DiGraph;
 
 pub fn solution(part: u8) -> usize {
-    let lines = include_str!("../../../problem_inputs_2020/day_7_test.txt");
+    let lines = include_str!("../../../problem_inputs_2020/day_7.txt");
     match part {
         1 => solve01(lines),
         2 => solve02(lines),
@@ -13,39 +13,63 @@ pub fn solution(part: u8) -> usize {
 }
 
 fn solve01(lines: &str) -> usize {
-    let mut color_hash = FxHashMap::<u32, &str>::default();
-    let mut bags = FxHashMap::<u32, Vec<u32>>::default();
+    let mut seen_colors = FxHashMap::default();
+    // Graph is a directed graph that connects holder bag from its contents
+    let mut graph = DiGraph::<String, ()>::new();
     for line in lines.lines() {
         let line = line.split(' ').collect_vec();
-        let (color_str, color) = (line[1], hash_color(line[1]));
-        color_hash.insert(color, color_str);
-        let mut contains = Vec::new();
+        let current_color = format!("{}{}", line[0], line[1]);
+        let current_color_node = if let Some(node) = seen_colors.get(&current_color.clone()) {
+            *node
+        } else {
+            graph.add_node(current_color.clone())
+        };
+        seen_colors.insert(current_color, current_color_node);
         if line[4] != "no" {
-            for i in (4..line.len()).step_by(4) {
-                let (color_str, color) = (line[i + 2], hash_color(line[i + 2]));
-                color_hash.insert(color, color_str);
-                contains.push(color);
+            for i in (5..line.len()).step_by(4) {
+                let color = format!("{}{}", line[i], line[i + 1]);
+                let color_node = if let Some(node) = seen_colors.get(&color.clone()) {
+                    *node
+                } else {
+                    graph.add_node(color.clone())
+                };
+                seen_colors.insert(color, color_node);
+                graph.add_edge(color_node, current_color_node, ());
             }
         }
-        bags.insert(color, contains);
     }
-    let keys = bags.keys().map(|f| color_hash.get(f).unwrap());
-    let values = bags
-        .values()
-        .map(|f| f.iter().map(|f| color_hash.get(f).unwrap()).collect_vec());
-    let zipped = keys.zip(values);
-    dbg!(zipped.collect_vec());
-    0
+    // dbg!(&graph);
+    let res = dijkstra(&graph, *seen_colors.get("shinygold").unwrap(), None, |_| 1).len();
+    res - 1
 }
 
 fn solve02(lines: &str) -> usize {
-    0
-}
-
-fn hash_color(color: &str) -> u32 {
-    let mut hash = 0;
-    for c in color.chars() {
-        hash += c as u32;
+    let mut seen_colors = FxHashMap::default();
+    // Graph is a directed graph that connects holder bag from its contents
+    let mut graph = DiGraph::<String, ()>::new();
+    for line in lines.lines() {
+        let line = line.split(' ').collect_vec();
+        let current_color = format!("{}{}", line[0], line[1]);
+        let current_color_node = if let Some(node) = seen_colors.get(&current_color.clone()) {
+            *node
+        } else {
+            graph.add_node(current_color.clone())
+        };
+        seen_colors.insert(current_color, current_color_node);
+        if line[4] != "no" {
+            for i in (5..line.len()).step_by(4) {
+                let color = format!("{}{}", line[i], line[i + 1]);
+                let color_node = if let Some(node) = seen_colors.get(&color.clone()) {
+                    *node
+                } else {
+                    graph.add_node(color.clone())
+                };
+                seen_colors.insert(color, color_node);
+                graph.add_edge(current_color_node, color_node, ());
+            }
+        }
     }
-    hash
+    // dbg!(&graph);
+    let res = dijkstra(&graph, *seen_colors.get("shinygold").unwrap(), None, |_| 1).len();
+    res - 1
 }
