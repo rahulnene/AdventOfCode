fa0e7a5d78cbfe2d2f9f8525f396b068555ac8e5
Rahul Nene <rahul.nene@live.com>
2023-12-29 15:11:04 +0530

    2018/4 complete

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use year_2018::days;
 fn main() {
     println!(
         "The solution to part 1 is: {}\n",
-        days::day3::solution(1)
+        days::day4::solution(1)
     );
     println!(
         "The solution to part 2 is: {}",
-        days::day3::solution(2)
+        days::day4::solution(2)
     );
 }
