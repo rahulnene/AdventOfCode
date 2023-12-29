d3987fc523bd2fbe3ac3ec71846f55b32f61dc8e
Rahul Nene <rahul.nene@live.com>
2023-12-29 16:22:27 +0530

    Day 5 finished

--- src/year_2018/days/day5.rs
+++ src/year_2018/days/day5.rs
@@ -1,16 +1,19 @@
 use itertools::Itertools;
 
+#[must_use]
 pub fn solution(part: u8) -> usize {
     let line = include_str!("../../../problem_inputs_2018/day_5.txt");
+    let polymer = Polymer(line.to_owned());
+
     match part {
-        1 => fully_react(line),
+        1 => fully_react(&polymer),
         2 => solve02(line),
         _ => 1,
     }
 }
 
-fn fully_react(polymer: &str) -> usize {
-    let mut polymer = Polymer(polymer.to_owned());
+fn fully_react(polymer: &Polymer) -> usize {
+    let mut polymer = polymer.clone();
     while let Some(next_poly) = polymer.react() {
         polymer.0 = next_poly;
     }
@@ -18,9 +21,18 @@ fn fully_react(polymer: &str) -> usize {
 }
 
 fn solve02(polymer: &str) -> usize {
-    let mut polymer = Polymer(polymer.to_owned());
-    dbg!(polymer.types());
-    0    
+    let types = Polymer(polymer.to_owned()).types();
+    let mut min_length = usize::MAX;
+    for typ in types.iter() {
+        let typeless_polymer: Polymer = Polymer(
+            polymer
+                .chars()
+                .filter(|c| c != typ && *c != typ.to_ascii_uppercase())
+                .collect(),
+        );
+        min_length = min_length.min(fully_react(&typeless_polymer));
+    }
+    min_length
 }
 
 #[derive(Clone, Debug)]
@@ -28,22 +40,19 @@ struct Polymer(String);
 
 impl Polymer {
     fn react(&self) -> Option<String> {
-        match self
-            .0
+        self.0
             .chars()
             .tuple_windows()
             .find(|(a, b)| (*a as u8).abs_diff(*b as u8) == 32_u8)
-        {
-            Some((a, b)) => {
-                Some(self.0.replacen(&format!("{a}{b}"), "", 1))
-            }
-            None => {
-                None
-            }
-        }
+            .map(|(a, b)| self.0.replacen(&format!("{a}{b}"), "", 1))
     }
 
     fn types(&self) -> Vec<char> {
-        return self.0.chars().flat_map(char::to_lowercase).unique().collect_vec();
+        return self
+            .0
+            .chars()
+            .flat_map(char::to_lowercase)
+            .unique()
+            .collect_vec();
     }
 }
