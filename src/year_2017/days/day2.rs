3f78c1dadf5ce603d810e02c30b767647ecb53f7
Rahul Nene <rahul.nene@systoolsgroup.com>
2024-01-04 15:11:10 +0530

    2017D2 Complete

--- src/year_2017/days/day2.rs
+++ src/year_2017/days/day2.rs
@@ -1,16 +1,36 @@
 pub fn solution(part: u8) -> usize {
     let lines = include_str!("../../../problem_inputs_2017/day_2.txt");
     match part {
-        1 => solve01(lines),
-        2 => solve02(lines),
+        1 => solve(lines, find_difference),
+        2 => solve(lines, find_quotient),
         _ => 1,
     }
 }
 
-fn solve01(lines: &str) -> usize {
+fn solve(lines: &str, f: impl Fn(&[usize]) -> usize) -> usize {
+    lines
+        .lines()
+        .map(|line| {
+            let nums = line
+                .split_whitespace()
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            f(&nums)
+        })
+        .sum()
+}
+
+fn find_quotient(nums: &[usize]) -> usize {
+    for (i, num) in nums.iter().enumerate() {
+        for (j, num2) in nums.iter().enumerate() {
+            if i != j && num % num2 == 0 {
+                return *num / *num2;
+            }
+        }
+    }
     0
 }
 
-fn solve02(lines: &str) -> usize {
-    0
+fn find_difference(nums: &[usize]) -> usize {
+    nums.iter().max().unwrap() - nums.iter().min().unwrap()
 }
