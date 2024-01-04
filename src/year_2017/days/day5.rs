a445ae728d474a7c0e7084d2b4a950ea4e5cd165
Rahul Nene <rahul.nene@systoolsgroup.com>
2024-01-04 15:32:12 +0530

    2017D5 Complete

--- src/year_2017/days/day5.rs
+++ src/year_2017/days/day5.rs
@@ -1,16 +1,21 @@
 pub fn solution(part: u8) -> usize {
     let lines = include_str!("../../../problem_inputs_2017/day_5.txt");
     match part {
-        1 => solve01(lines),
-        2 => solve02(lines),
+        1 => solve(lines, |_| 1),
+        2 => solve(lines, |f| if f >= 3 { -1 } else { 1 }),
         _ => 1,
     }
 }
 
-fn solve01(lines: &str) -> usize {
-    0
-}
-
-fn solve02(lines: &str) -> usize {
-    0
+fn solve(lines: &str, f: impl Fn(isize) -> isize) -> usize {
+    let mut offsets: Vec<isize> = lines.lines().map(|f| f.parse::<isize>().unwrap()).collect();
+    let mut pointer: isize = 0;
+    let mut steps = 0;
+    while pointer >= 0 && (pointer as usize) < offsets.len() {
+        let offset = offsets[pointer as usize];
+        offsets[pointer as usize] += f(offset);
+        pointer = pointer + offset;
+        steps += 1;
+    }
+    steps
 }
