20f83d8c3416db241ebb1950d8ba7d1fc2cef17c
Rahul Nene <rahul.nene@systoolsgroup.com>
2024-01-04 15:21:28 +0530

    2017D4 Complete

--- src/year_2017/days/day4.rs
+++ src/year_2017/days/day4.rs
@@ -1,16 +1,28 @@
+use itertools::Itertools;
+
 pub fn solution(part: u8) -> usize {
-    let lines = include_str!("../../../problem_inputs_2017/day_2.txt");
+    let lines = include_str!("../../../problem_inputs_2017/day_4.txt");
     match part {
-        1 => solve01(lines),
-        2 => solve02(lines),
+        1 => solve(lines, part_1),
+        2 => solve(lines, part_2),
         _ => 1,
     }
 }
 
-fn solve01(lines: &str) -> usize {
-    0
+fn solve(lines: &str, is_valid: impl Fn(&str) -> usize) -> usize {
+    lines
+        .lines()
+        .filter(|line| is_valid(line) == line.split(' ').count())
+        .count()
 }
 
-fn solve02(lines: &str) -> usize {
-    0
+fn part_1(line: &str) -> usize {
+    line.split(' ').unique().count()
+}
+
+fn part_2(line: &str) -> usize {
+    line.split(' ')
+        .map(|f| f.chars().sorted_unstable().collect::<String>())
+        .unique()
+        .count()
 }
