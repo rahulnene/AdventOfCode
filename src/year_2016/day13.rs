13529964fd61650eebad36102eb356cc8bc8081a
Rahul Nene <rahul.nene@systoolsgroup.com>
2024-03-01 11:20:25 +0530

    2016D17 finished

--- src/year_2016/day13.rs
+++ src/year_2016/day13.rs
@@ -1,12 +1,101 @@
-pub fn solution() -> (usize, usize) {
-    let lines = include_str!("../../../problem_inputs_2017/day_13.txt");
-    (solve01(lines), solve02(lines))
+use std::{
+    collections::VecDeque,
+    time::{Duration, Instant},
+};
+
+use rustc_hash::FxHashMap;
+
+pub fn solution() -> ((usize, Duration), (usize, Duration)) {
+    (solve01(), solve02())
 }
 
-fn solve01(lines: &str) -> usize {
-    0
+fn solve01() -> (usize, Duration) {
+    let now = Instant::now();
+
+    let mut maze = Maze::new(1350);
+    let ans = maze.find_path_to((31, 39)).unwrap();
+    // let mut maze = Maze::new(10);
+    // let ans = maze.find_path_to((7, 4)).unwrap();
+    (ans, now.elapsed())
 }
 
-fn solve02(lines: &str) -> usize {
-    0
+fn solve02() -> (usize, Duration) {
+    let now = Instant::now();
+
+    let mut maze = Maze::new(1350);
+    // let ans = maze.find_all_locations_within(50);
+    (0, now.elapsed())
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Content {
+    Wall,
+    Open,
+}
+
+type Position = (isize, isize);
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Maze {
+    favorite_number: u16,
+    contents: FxHashMap<Position, Content>,
+}
+
+impl Maze {
+    fn new(favorite_number: u16) -> Self {
+        Self {
+            favorite_number,
+            contents: FxHashMap::default(),
+        }
+    }
+
+    fn get_content(&mut self, position: Position) -> Content {
+        if let Some(content) = self.contents.get(&position) {
+            return *content;
+        }
+        let (x, y) = position;
+        let value = x * x + 3 * x + 2 * x * y + y + y * y + self.favorite_number as isize;
+        let is_open = value.count_ones() % 2 == 0;
+        let content = if is_open {
+            Content::Open
+        } else {
+            Content::Wall
+        };
+        self.contents.insert(
+            position,
+            if is_open {
+                Content::Open
+            } else {
+                Content::Wall
+            },
+        );
+        content
+    }
+
+    fn find_path_to(&mut self, target: Position) -> Option<usize> {
+        let mut queue = VecDeque::new();
+        queue.push_front((1, 1, 0));
+        let mut visited = FxHashMap::default();
+        visited.insert((1, 1), 0);
+        while let Some((x, y, steps)) = queue.pop_back() {
+            if (x, y) == target {
+                return Some(steps);
+            }
+            for (dx, dy) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                let new_x = x + dx;
+                let new_y = y + dy;
+                if new_x < 0 || new_y < 0 {
+                    continue;
+                }
+                if visited.contains_key(&(new_x, new_y)) {
+                    continue;
+                }
+                let content = self.get_content((new_x, new_y));
+                if content == Content::Wall {
+                    continue;
+                }
+                visited.insert((new_x, new_y), steps + 1);
+                queue.push_front((new_x, new_y, steps + 1));
+            }
+        }
+        None
+    }
 }
--- src/year_2016/day17.rs
+++ src/year_2016/day17.rs
@@ -1,12 +1,78 @@
-pub fn solution() -> (usize, usize) {
-    let lines = include_str!("../../../problem_inputs_2016/day_.txt");
-    (solve01(lines), solve02(lines))
+use md5::compute;
+use std::{
+    collections::VecDeque,
+    time::{Duration, Instant},
+};
+pub fn solution() -> ((String, Duration), (usize, Duration)) {
+    let now = Instant::now();
+    let start_hash = "ioramepc";
+    let ans = get_shortest_and_longest_paths(&now, start_hash);
+    ans
 }
 
-fn solve01(lines: &str) -> usize {
-    0
+type Position = (isize, isize);
+
+//[up, down, left, right]
+type OpenDoors = [bool; 4];
+
+fn get_open_doors(pos: &Position, hash_str: &str) -> OpenDoors {
+    let hash = compute(hash_str.as_bytes());
+    let hash = format!("{:x}", hash);
+    let hash = hash.chars().take(4).collect::<String>();
+
+    let mut doors = [false; 4];
+    for (i, c) in hash.chars().enumerate() {
+        doors[i] = c > 'a' && c <= 'f';
+    }
+    if pos.0 == 0 {
+        doors[2] = false;
+    }
+    if pos.1 == 0 {
+        doors[0] = false;
+    }
+    if pos.0 == 3 {
+        doors[3] = false;
+    }
+    if pos.1 == 3 {
+        doors[1] = false;
+    }
+    doors
 }
 
-fn solve02(lines: &str) -> usize {
-    0
+fn get_shortest_and_longest_paths(
+    start_time: &Instant,
+    start_hash: &str,
+) -> ((String, Duration), (usize, Duration)) {
+    let mut longest_path = 0;
+    let mut stack = VecDeque::new();
+    let mut shortest_path = String::default();
+    let mut time_p1 = Duration::default();
+    stack.push_back((0, 0, start_hash.to_string()));
+    while let Some((x, y, hash)) = stack.pop_front() {
+        if x == 3 && y == 3 {
+            if shortest_path.len() == 0 {
+                shortest_path = hash[start_hash.len()..].to_string();
+                time_p1 = start_time.elapsed();
+            }
+            longest_path = longest_path.max(hash[start_hash.len()..].len());
+        } else {
+            let doors = get_open_doors(&(x, y), &hash);
+            if doors[0] {
+                stack.push_back((x, y - 1, format!("{}{}", hash, "U")));
+            }
+            if doors[1] {
+                stack.push_back((x, y + 1, format!("{}{}", hash, "D")));
+            }
+            if doors[2] {
+                stack.push_back((x - 1, y, format!("{}{}", hash, "L")));
+            }
+            if doors[3] {
+                stack.push_back((x + 1, y, format!("{}{}", hash, "R")));
+            }
+        }
+    }
+    (
+        (shortest_path, time_p1),
+        (longest_path, start_time.elapsed()),
+    )
 }
