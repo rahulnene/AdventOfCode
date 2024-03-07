61bbd80161dea0e3a65adcc1d987f15d37e977cc
Rahul Nene <rahul.nene@systoolsgroup.com>
2024-03-07 16:12:09 +0530

    .

--- src/year_2015/day24.rs
+++ src/year_2015/day24.rs
@@ -1,15 +1,51 @@
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
+
+use itertools::Itertools;
+
+const LINES: &str = include_str!("../../problem_inputs_2015/day_24.txt");
+
 pub fn solution() -> ((usize, Duration), (usize, Duration)) {
-    let lines = include_str!("../../../problem_inputs_2015/day_24.txt");
-    (solve01(&lines), solve02(&lines))
+    let packages = LINES
+        .lines()
+        .map(|l| l.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    (solve01(&packages), solve02(&packages))
 }
 
-fn solve01(lines: &str) -> (usize, Duration) {
+fn solve01(packages: &[usize]) -> (usize, Duration) {
     let now = Instant::now();
-    (0, now.elapsed())
+    let ans = get_min_qe_for_n_parts(packages, 3);
+    (ans, now.elapsed())
 }
 
-fn solve02(lines: &str) -> (usize, Duration) {
+fn solve02(packages: &[usize]) -> (usize, Duration) {
     let now = Instant::now();
-    (0, now.elapsed())
+    let ans = get_min_qe_for_n_parts(packages, 4);
+    (ans, now.elapsed())
+}
+
+fn quantum_entanglement(packages: &[&usize]) -> usize {
+    packages.into_iter().copied().product()
+}
+
+fn get_min_qe_for_n_parts(packages: &[usize], n: usize) -> usize {
+    let total_weight: usize = packages.iter().sum();
+    let target_weight = total_weight / n;
+    let possible_combos = (1..packages.len())
+        .map(|combo_lenths| {
+            packages
+                .iter()
+                .combinations(combo_lenths)
+                .filter(|combo| combo.into_iter().copied().sum::<usize>() == target_weight)
+                .collect::<Vec<_>>()
+        })
+        .filter(|l| l.len() > 0)
+        .next()
+        .unwrap();
+    let ans = possible_combos
+        .iter()
+        .map(|v| quantum_entanglement(&v))
+        .min()
+        .unwrap();
+    ans
 }
